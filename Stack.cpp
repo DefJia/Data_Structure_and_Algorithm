b62b4ac73a3dc65f833360582c34ceb97dcd02cf
贾泽瑞 <defjiachina@gmail.com>
2018-07-30 23:30:40 +0800

    unknown change

--- Stack.cpp
+++ Stack.cpp
@@ -1,59 +1,53 @@
-#include <stdio.h> 
-#include <cstdlib> 
-#include <stdlib.h> 
-#include <string.h>
-#define INIT_SIZE 1
-#define INCREMENT 1 
- 
-typedef char ELEM; 
- 
-typedef struct{ 
-   	ELEM *top; 
-    ELEM *base; 
-    int size; 
-}Stack; 
- 
+#include <stdio.h>
+#include <cstdlib>
+#define INIT_SIZE 100
+#define INCREMENT 10
+
+typedef unsigned int ELEM;
+
+typedef struct{
+	ELEM *top;
+	ELEM *base;
+	int size;
+}Stack;
+
 void InitStack(Stack &S){
-   S.base = (ELEM *)malloc(INIT_SIZE * sizeof(ELEM)); 
- if(!S.base){exit(0);} 
-  S.top = S.base; 
-    S.size = INIT_SIZE; 
-} 
- 
-char GetTop(Stack &S){ 
-  if(S.top == S.base){ 
-       printf("stack is empty!\n"); 
-       return '?'; 
-    } 
-  char e = *(S.top - 1); 
- return e; 
-} 
- 
-void Push(Stack &S, ELEM e){ 
-  if(S.top - S.base >= S.size){ 
-       S.base = (ELEM *)realloc(S.base, (S.size + INCREMENT) * sizeof(ELEM)); 
-     if(!S.base)exit(0); 
-        S.top = S.base + S.size;//deletable 
-        S.size += INCREMENT; 
-   } 
-  *S.top++ = e;//指针加减 
-} 
- 
-char Pop(Stack &S){ 
- if(S.top == S.base)return '?'; 
- ELEM e; 
-    e = *--S.top; 
-  return e; 
-  //printf("%c", e); 
+	S.base = (ELEM *)malloc(INIT_SIZE * sizeof(ELEM));
+	if(!S.base){exit(0);}
+	S.top = S.base;
+	S.size = INIT_SIZE;
 }
 
-int main(){ 
- Stack test; 
-    InitStack(test); 
-    Push(test, 1); 
-    Push(test, 2); 
-    Push(test, 3); 
-    ELEM e; 
-    Pop(test); 
-  printf("%d\n", e); 
+void GetTop(Stack &S, ELEM &e){
+	if(S.top == S.base){
+		printf("stack is empty!\n");
+		return ;
+	}
+	e = *(S.top - 1);
+}
+
+void Push(Stack &S, ELEM e){
+	if(S.top - S.base >= S.size){
+		S.base = (ELEM *)realloc(S.base, (S.size + INCREMENT) * sizeof(ELEM));
+		if(!S.base)exit(0);
+		S.top = S.base + S.size;//deletable
+		S.size += INCREMENT;
+	}
+	*S.top++ = e;//指针加减
+}
+
+void Pop(Stack &S, ELEM &e){
+	if(S.top == S.base)return;
+	e = *--S.top;
+}
+
+int main(){
+	Stack test;
+	InitStack(test);
+	Push(test, 1);
+	Push(test, 2);
+	Push(test, 3);
+	ELEM e;
+	Pop(test, e);
+	printf("%d\n", e);
 }
