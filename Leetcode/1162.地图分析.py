2d6d239ecb1be190a1cb29433cc49df9de7728a9
Defjia <code@defjia.top>
2020-03-30 00:00:20 +0800

    :see_no_evil:Updating .gitignore

---
+++ Leetcode/1162.地图分析.py
@@ -0,0 +1,26 @@
+#
+# @lc app=leetcode.cn id=1162 lang=python3
+#
+# [1162] 地图分析
+#
+
+# @lc code=start
+class Solution:
+    @staticmethod
+    def find_neiborhod(cur, n):
+        return lst
+
+    def maxDistance(self, grid):
+        ground_num = cnt = 0
+        rest = dict()
+        l = len(grid)
+        for i in range(l):
+            for j in range(l):
+                rest[i * l + j] = grid[i][j]
+                ground_num += 1 if grid[i][j] == 1
+        
+
+        
+
+# @lc code=end
+
